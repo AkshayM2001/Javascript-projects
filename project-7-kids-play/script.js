3dd91a06c7314bb11a423ed6c5c376d6e493ae4f
AkshayM2001 <akshaymatale2001@gmail.com>
2023-08-06 22:50:20 +0530

    addinm functinality

---
+++ project-7-kids-play/script.js
@@ -0,0 +1,30 @@
+const formEl = document.getElementById("form");
+const scoreEl = document.getElementById("score");
+const textEl = document.getElementById("text");
+const inputEl = document.getElementById("input1");
+const btnEl = document.getElementById("btn");
+
+/// GENERATING RANDOM TWO NUMBERS 
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+// INJECTING IN LIVE PAGE
+textEl.innerText = `What is ${num1} multiply by ${num2} ?`;
+
+// VALIDATING ANSWER
+const correctAns = num1 * num2;
+
+let score = 0;
+
+// ADDING EVENT LISTENER TO BTN AND GETTING INPUT IN NUMBER NOT STRING
+
+btnEl.addEventListener("submit", () => {
+    const userAns = +inputEl.Value;
+    if(score === correctAns){
+        score++;
+        console.log(score)
+    }
+    else{
+        score--;
+        console.log(score)
+    }
+});
