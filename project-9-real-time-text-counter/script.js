985a706beee6b14f67c38a3d5bb4e471c364d9fc
AkshayM2001 <akshaymatale2001@gmail.com>
2023-08-08 21:50:56 +0530

    completed

--- project-9-real-time-text-counter/script.js
+++ project-9-real-time-text-counter/script.js
@@ -1,7 +1,15 @@
-const text = document.getElementById("text");
+const textEl = document.getElementById("text");
+const countEl = document.getElementById("count");
+const remainEl = document.getElementById("remain");
 
 
-text.addEventListener("KeyboardEvent", () => {
-    
-    
+textEl.addEventListener("keyup", () => {
+    updatecounter();
 });
+
+function updatecounter() {
+    countEl.innerText = textEl.value.length;
+    remainEl.innerText = textEl.getAttribute("maxLength") - textEl.value.length;
+};
+
+updatecounter();
